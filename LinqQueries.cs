fa94937f4cc8019aedcdc16c501b71c3de742957
Oswaldo Sierra <oswaldox199@gmail.com>
2026-06-18 11:32:53 -0500

    Implementar la clase LinqQueries para cargar y devolver la colección de libros

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -1,4 +1,19 @@
+using System.Text.Json;
+
 public class LinqQueries
 {
-  public LinqQueries(){}
+  private List<Book> librosCollection = new List<Book>();
+  public LinqQueries()
+  {
+    using(StreamReader reader = new StreamReader("books.json"))
+    {
+      string json = reader.ReadToEnd();
+      this.librosCollection = JsonSerializer.Deserialize<List<Book>>(json, new JsonSerializerOptions() {PropertyNameCaseInsensitive = true});
+    }
+  }
+
+  public IEnumerable<Book> TodaLaColeccion()
+  {
+    return librosCollection;
+  }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Hello, World!");
+
+LinqQueries queries = new();
+ImprimirValores(queries.TodaLaColeccion());
+void ImprimirValores(IEnumerable<Book> listadelibros)
+{
+  Console.WriteLine("{0, -60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha Publicacion");
+  foreach (var book in listadelibros)
+  {
+    Console.WriteLine("{0, -60} {1, 15} {2, 15}", book.Title, book.PageCount, book.PublishedDate.ToShortDateString());
+  }
+}
